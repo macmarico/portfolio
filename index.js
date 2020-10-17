a015b4691be44273ef361449f3dfcd33cda99c29
manish chandra <manishchandra012@gmail.com>
2020-10-17 16:16:57 +0530

    making it a node project
    added sever capablity and package.json

---
+++ index.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const lookup = require('mime-types').lookup;
+
+const server = http.createServer((req, res)=>{
+    console.log('thankyou for requesting');
+    let parsedURL = url.parse(req.url, true);
+    let path = parsedURL.path.replace(/^\/+|\/+$/g, "");
+
+    if(path == ''){
+        path = 'index.html';
+    }
+    console.log(`requested path is ${path}`);
+
+    let file = __dirname + '/portfolio/' + path;
+    fs.readFile(file, (err, content)=>{
+        if(err){
+            console.log(`file not found ${file}`);
+            res.writeHead(404);
+            res.end();
+        } else{
+            console.log(`returning file ${file}`);
+            res.setHeader('X-Content-Type-Option', 'nosniff');
+            let mime = lookup(path);
+            res.writeHead(200, {'Content-type': mime});
+            res.end(content);
+        }
+
+    });
+
+
+});
+
+server.listen(3000,()=>{
+    console.log('listening to port 3000')
+});
